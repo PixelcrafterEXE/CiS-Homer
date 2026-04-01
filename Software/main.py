6585d92eef8faf54dd569f17e39191218b24eef4
Pixelcrafter <michischaedel@gmail.com>
2026-04-01 16:10:19 +0200

    reverted testing changes

--- Software/main.py
+++ Software/main.py
@@ -2,11 +2,8 @@ from lib.UI import UI
 from lib.Sensor import Sensor
 
 def main():
-    #ui = UI()
-    #ui.mainloop()
-    S = Sensor()
-    S.writeGain(1)
-    print(S.getGain())
+    ui = UI()
+    ui.mainloop()
 
 if __name__ == "__main__":
     main()
